76832d3175c4738c4bd337ee9a9c2b28c1bc8b68
Thiago Ribeiro Fantin <thiribeiro@yahoo.com>
2025-12-30 11:59:12 -0300

    x

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -7,7 +7,7 @@ export const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT) || 13476,
+    port: 13476,
     ssl: {
         rejectUnauthorized: false
     },
