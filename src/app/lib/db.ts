5c55f0915b847d6e832740e5860f29227e3e162b
Thiago Ribeiro Fantin <thiribeiro@yahoo.com>
2025-12-30 11:50:35 -0300

    red

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -3,11 +3,11 @@ import mysql from 'mysql2/promise';
 // Criamos uma "Pool" (Piscina) de conexões
 // Ela gerencia as conexões automaticamente para você
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'thiagorf',
-    password: process.env.DB_PASSWORD || 'teadoro123',
-    database: process.env.DB_NAME || 'controle_validades',
-    port: Number(process.env.DB_PORT),
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 13476,
     ssl: {
         rejectUnauthorized: false
     },
