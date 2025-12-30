48cbebf7984797814be6ac4e043059169a59d7a4
Thiago Ribeiro Fantin <thiribeiro@yahoo.com>
2025-12-30 11:25:45 -0300

    aiven

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -3,10 +3,14 @@ import mysql from 'mysql2/promise';
 // Criamos uma "Pool" (Piscina) de conexões
 // Ela gerencia as conexões automaticamente para você
 export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'thiagorf',
-    password: 'teadoro123',
-    database: 'controle_validades',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'thiagorf',
+    password: process.env.DB_PASSWORD || 'teadoro123',
+    database: process.env.DB_NAME || 'controle_validades',
+    port: Number(process.env.DB_PORT),
+    ssl: {
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10, // Limite de conexões simultâneas
     queueLimit: 0
